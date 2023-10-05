088dbddba1fc3f4ceb1b57b9b5263200e819cade
Leonardo Lemos <leonardo.lemos2002@hotmail.com>
2023-10-05 11:07:27 -0300

    :feat: criando pagina de conectar ao calendario google

---
+++ src/pages/api/users/index.api.ts
@@ -0,0 +1,44 @@
+/* eslint-disable prettier/prettier */
+import { prisma } from '@/lib/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
+
+type IRequest = {
+  username: string
+  name: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const { name, username } = req.body as IRequest
+
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({ message: 'Username alread exists' })
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      username,
+    },
+  })
+
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
+  })
+
+  return res.status(201).json(user)
+}
